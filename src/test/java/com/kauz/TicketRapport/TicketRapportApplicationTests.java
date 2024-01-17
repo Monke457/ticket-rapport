e21ecf43a4ba3699176a40ddf5f7c9e8f74191fa
Beth <beth.williams@bluewin.ch>
2024-01-17 13:20:32 +0100

    Initial Commit

---
+++ src/test/java/com/kauz/TicketRapport/TicketRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauz.TicketRapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class TicketRapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
