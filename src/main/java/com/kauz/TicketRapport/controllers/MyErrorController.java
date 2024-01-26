a165a6d3727099791c7418db73c30c8ec796bf33
Beth <beth.williams@bluewin.ch>
2024-01-26 16:34:52 +0100

    feat: added error handling

---
+++ src/main/java/com/kauz/TicketRapport/controllers/MyErrorController.java
@@ -0,0 +1,35 @@
+package com.kauz.TicketRapport.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
+
+/**
+ * A custom error handling controller.
+ * Returns access denied message to 403 errors and redirects to the login if a database user cannot be found using session data.
+ * All other errors show a styled generic error message.
+ */
+@Controller
+public class MyErrorController extends BaseController implements ErrorController {
+
+    @GetMapping("/error")
+    public String error(Model model, HttpServletRequest request) {
+        if (authUser.getUser() == null) return "redirect:/login";
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status == null || !Objects.equals(status.toString(), "403")) {
+            model.addAttribute("message", status + " Oops! Something went wrong.");
+        } else {
+            model.addAttribute("message", "You are not permitted to access this page.");
+        }
+
+        super.addBaseAttributes(model);
+        return "error";
+    }
+}
